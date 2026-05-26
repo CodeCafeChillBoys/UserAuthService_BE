fcf1d6c3b1d952294f3323c177766d0834f45b55
VoVanCuong <cuongcon2k04@gmail.com>
2026-05-26 14:31:22 +0700

    [Update] File config DI

---
+++ UserAuthService.API/Configurations/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAuthService.API.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- UserAuthService.API/Program.cs
+++ UserAuthService.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddDependencyInjection();
 builder.Services.AddSwaggerGen(options =>
 {
     options.OperationFilter<LowercaseQueryParameterFilter>();
